39896759b7a6cac4678dd0f09bdb88cee9d1127c
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2024-12-24 11:49:53 +0300

    created initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
-  return (
-    <div>
+"use client"
+import createRandomBoard from "@/utils/CreateRandomBoard";
+import DisplayBoard from "@/components/board/DisplayBoard";
+import { useEffect, useState } from "react";
 
+export default function Home() {
+  const boardSize = 10;
+  const [board, setBoard] = useState<boolean[][]>([]);
+  useEffect(() => {
+    setBoard(createRandomBoard(boardSize));
+  }, []);
+
+  return (
+    <div className="flex items-center justify-center flex-col">
+      <DisplayBoard board={board}/>
     </div>
   );
 }
