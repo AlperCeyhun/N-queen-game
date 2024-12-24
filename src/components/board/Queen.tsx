bca692e57b6e91e5c45c4ea6de2da2653d70ad36
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2024-12-24 11:50:35 +0300

    created initial Queen.tsx

---
+++ src/components/board/Queen.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+const Queen: React.FC = () => {
+
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleHover = (hovering: boolean) => {
+    setIsHovered(hovering);
+    console.log(hovering ? "Hovering over the queen!" : "Not hovering.");
+  };
+  return (
+    <div className="flex justify-center items-center h-full z-10"
+    onMouseEnter={() => handleHover(true)}
+    onMouseLeave={() => handleHover(false)}>
+      <span className="text-5xl text-black font-bold hover:">♕</span>
+    </div>
+  );
+};
+
+export default Queen;
