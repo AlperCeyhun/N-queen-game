9bb95a69dbba90b91f4777b78782770776293fa6
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2024-12-26 12:48:54 +0300

    removed unnecesary variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,6 @@ import CreateEmptyBoard from "@/utils/CreateEmptyBoard";
 export default function Home() {
   const boardSize = 10;
   const [board, setBoard] = useState<boolean[][]>([]);
-  
-  const testBoard = CreateEmptyBoard(boardSize);
 
   useEffect(() => {
     setBoard(createRandomBoard(boardSize));
@@ -19,7 +17,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col">
       <DisplayBoard board={board}/>
-      <TestDisplayBoard board={getQueenRange({ board: testBoard, row: 2, col: 2})} />
+      <TestDisplayBoard board={getQueenRange({ board: CreateEmptyBoard(boardSize), row: 2, col: 2})} />
     </div>
   );
 }
