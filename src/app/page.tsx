a00480ffdb6f108b89c62f1c2751efbd055ed211
Alper Ceyhun <80386511+AlperCeyhun@users.noreply.github.com>
2024-12-26 12:34:44 +0300

    added test component for queenRange

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@
 import createRandomBoard from "@/utils/CreateRandomBoard";
 import DisplayBoard from "@/components/board/DisplayBoard";
 import { useEffect, useState } from "react";
+import TestDisplayBoard from "@/components/test/TestDisplayBoard";
+import getQueenRange from "@/utils/queenRange/GetQueenRange";
+import CreateEmptyBoard from "@/utils/CreateEmptyBoard";
 
 export default function Home() {
   const boardSize = 10;
   const [board, setBoard] = useState<boolean[][]>([]);
+  
+  const testBoard = CreateEmptyBoard(boardSize);
+
   useEffect(() => {
     setBoard(createRandomBoard(boardSize));
   }, []);
@@ -13,6 +19,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center flex-col">
       <DisplayBoard board={board}/>
+      <TestDisplayBoard board={getQueenRange({ board: testBoard, row: 2, col: 2})} />
     </div>
   );
 }
